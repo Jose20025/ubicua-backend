29e71e0ee013ffc5e3482ef291053e8bd2d54c55
Jose Carlos Gutiérrez <joseca20025@gmail.com>
2024-03-13 20:11:56 -0400

    Agregado PostDTO para manejar los datos de las publicaciones

---
+++ blog-api/src/main/java/com/joseca/blogapi/dto/PostDTO.java
@@ -0,0 +1,12 @@
+package com.joseca.blogapi.dto;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+    private String title;
+
+    private String description;
+
+    private String content;
+}
